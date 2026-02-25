b2b41fcbe419008b82fac3bfd5e768f970f1e2cf
Firdovsirz <firdovsirz@gmail.com>
2026-02-25 12:26:01 +0400

    deploy

--- src/util/apiClient.ts
+++ src/util/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { RootState, store } from "../redux/store";
 
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'http://5.189.136.206:8010';
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
